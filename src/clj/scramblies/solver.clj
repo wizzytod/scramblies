f038acc781bd88ad2c8fa7a78e304c0e5d737e1f
Vitaly Kornilov <wizzytod@gmail.com>
2018-08-10 09:22:35 +0300

    reduce replaced with frequencies

--- src/clj/scramblies/solver.clj
+++ src/clj/scramblies/solver.clj
@@ -2,7 +2,7 @@
 
 (defn scramble? [source pattern]
   (and  (>= (count source) (count pattern))
-        (let [char-map (reduce (fn [acc n] (update acc n (fnil inc 0) )) {} pattern)]
+        (let [char-map (frequencies pattern)]
           (->> source
                (reduce (fn [acc n] (update acc n (fnil dec 0))) char-map)
                (not-any? (fn [[_ v]] (pos? v)))))))
